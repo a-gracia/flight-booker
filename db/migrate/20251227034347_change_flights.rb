3add386fa2c2152d199b6e89d21959948f54af09
Andres <18741308+a-gracia@users.noreply.github.com>
2025-12-27 00:42:00 -0500

    feat: Add flight search bar

--- db/migrate/20251227034347_change_flights.rb
+++
@@ -1,6 +0,0 @@
-class ChangeFlights < ActiveRecord::Migration[8.1]
-  change_table :flights do |t|
-    t.rename :arrival_airport, :arrival_airport_id
-    t.rename :departure_airport, :departure_airport_id
-  end
-end
